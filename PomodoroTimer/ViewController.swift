40dc2b6d65ab4b74d4a8a23b62766d13f8a1f76e
Timur Baklanov <fkacharlie@gmail.com>
2020-02-10 21:52:02 +0300

    I created a Timer instance

--- PomodoroTimer/ViewController.swift
+++ PomodoroTimer/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var startTimerButton: UIButton!
+
+    var timer = Timer()
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor(red: 255/255, green: 240/255, blue: 255/255, alpha: 1)
     }
-
-
+    
+    @IBAction func startTimer(_ sender: UIButton) {
+        if timer.isValid {
+            return
+        } else {
+             timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(setLabel), userInfo: nil, repeats: true)
+            sender.isEnabled = false
+        }
+    }
+    @IBAction func finishTimer(_ sender: UIButton) {
+        count = 0
+        timeLabel.text = "0"
+        timer.invalidate()
+        startTimerButton.isEnabled = true
+    }
+    
+    @objc func setLabel() {
+        count += 1
+        timeLabel.text = String(count)
+    }
 }
-
